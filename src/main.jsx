f5cf65755ea87932b1ccb8350aeb6499713af0b2
LaZyFee <rhr277@gmail.com>
2025-08-19 12:19:37 +0600

    Fix case-sensitive import for Router/Routes.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Router/routes.jsx";
 import "swiper/css";
 import "swiper/css/pagination";
 import "react-datepicker/dist/react-datepicker.css";
+import { router } from "./Router/routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
