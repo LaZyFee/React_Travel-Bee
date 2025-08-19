8006e9963704494ab95366fe4ad0884033e71b10
LaZyFee <rhr277@gmail.com>
2025-08-19 12:15:00 +0600

    Fix case-sensitive import for Router/Routes.jsx

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -35,10 +35,6 @@ export const router = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
         path: "/about",
         element: <About />,
       },
